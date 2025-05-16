2e81c1c25909b298538d151d83cca13ea1c16abc
Gabriel-webmaster <gabrielalfredocuna9@gmail.com>
2025-05-16 04:51:44 -0700

    add task

---
+++ gestor de tarefas/src/assets/components/main.tsx
@@ -0,0 +1,10 @@
+export function Main() {
+    return(
+        <div className="h-screen w-screen bg-blue-500">
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            
+        </div>
+    )
+}
